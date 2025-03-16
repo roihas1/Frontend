ff00447d75b77f983346c82adf4d7a1308dc6cf8
Hass <roihas@post.bgu.ac.il>
2025-03-16 13:30:57 +0200

    fixing submit button

--- src/components/Layout/MissinigBets.tsx
+++ src/components/Layout/MissinigBets.tsx
@@ -78,7 +78,9 @@ const MissingBets = () => {
       const response = await axiosInstance.get(`/series/bets/allMissingBets`);
 
       setMissiningBetsData(response.data);
-      const totalMissingBets: number = Object.values(response.data as MissingBetsData ).reduce(
+      const totalMissingBets: number = Object.values(
+        response.data as MissingBetsData
+      ).reduce(
         (acc: number, series) =>
           acc +
           (series.gamesAndWinner ? 1 : 0) + // Add 1 if gamesAndWinner is true
@@ -95,7 +97,6 @@ const MissingBets = () => {
     if (isLoggedIn) {
       fetchMissingBets();
     }
-   
   }, [refreshTrigger]);
 
   useEffect(() => {
@@ -120,35 +121,43 @@ const MissingBets = () => {
 
   return (
     <div className="block ">
-      <IconButton onClick={handleClick} disabled={!isLoggedIn}>
-        <Badge
-          badgeContent={!isLoggedIn ? 0 : badgeContent}
-          sx={{
-            "& .MuiBadge-badge": {
-              backgroundColor: "#1D428A",
-              color: "white",
-              cursor: "pointer",
-            },
-          }}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className={`size-6 text-black ${
-              !isLoggedIn ? "text-gray-500" : ""
-            }`}
+      <div className="flex items-center">
+        <IconButton onClick={handleClick} disabled={!isLoggedIn}>
+          <Badge
+            badgeContent={!isLoggedIn ? 0 : badgeContent}
+            sx={{
+              "& .MuiBadge-badge": {
+                backgroundColor: "#1D428A",
+                color: "white",
+                cursor: "pointer",
+              },
+            }}
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
-            />
-          </svg>
-        </Badge>
-      </IconButton>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className={`size-6 text-black ${
+                !isLoggedIn ? "text-gray-500" : ""
+              }`}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
+              />
+            </svg>
+          </Badge>
+        </IconButton>
+
+        
+        <span className=" text-md text-gray-900 sm:hidden">
+          Missing Bets
+        </span>
+      </div>
+
       <Menu
         id="basic-menu"
         anchorEl={anchorEl}
--- src/components/common/SubmitButton.tsx
+++ src/components/common/SubmitButton.tsx
@@ -1,17 +1,30 @@
 interface SubmitButtonProps {
   loading: boolean;
   text: string;
-  onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void; // Type onClick to accept a mouse event
+  onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
   disabled?: boolean;
-  className?:string;
+  className?: string;
 }
 
-const SubmitButton: React.FC<SubmitButtonProps> = ({ loading, text, onClick , disabled , className}) => (
+const SubmitButton: React.FC<SubmitButtonProps> = ({
+  loading,
+  text,
+  onClick,
+  disabled,
+  className,
+}) => (
   <button
     type="submit"
-    className={`${className ?className :"w-1/3"} p-3  bg-colors-nba-blue text-white rounded-2xl hover:bg-colors-nba-red shadow-2xl `}
-    onClick={onClick} // onClick is passed from the parent component
-    disabled= {disabled}
+    className={`
+      ${className ? className : "w-full sm:w-1/3"} 
+      px-6 py-3 min-w-[120px] text-lg font-semibold 
+      bg-colors-nba-blue text-white rounded-xl 
+      hover:bg-colors-nba-red focus:ring-4 focus:ring-colors-nba-red/50 
+      disabled:bg-gray-400 disabled:cursor-not-allowed 
+      transition-all duration-200 ease-in-out shadow-lg
+    `}
+    onClick={onClick}
+    disabled={disabled}
   >
     {loading ? `${text}...` : text}
   </button>
